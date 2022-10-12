2b6965a322afa29a0c9f2aab52ec8ae2f1730c7b
Dowland Aiello <dowlandaiello@gmail.com>
2022-10-12 16:57:32 -0700

    Add FFI binding for spawning actors.

--- src/actor/mod.rs
+++ src/actor/mod.rs
@@ -11,6 +11,9 @@ extern "C" {
 
 	#[link_name = "address"]
 	fn address_raw() -> Address;
+
+	#[link_name = "spawn_actor"]
+	fn spawn_actor_raw(addr: Address) -> Address;
 }
 
 pub fn send_message(addr: Address, msg_name_buf: WasmPtr<u8, Array>, msg_buf: WasmPtr<u8, Array>) {
@@ -19,4 +22,8 @@ pub fn send_message(addr: Address, msg_name_buf: WasmPtr<u8, Array>, msg_buf: Wa
 
 pub fn address() -> Address {
 	unsafe { address_raw() }
+}
+
+pub fn spawn_actor(addr: Address) -> Address {
+	unsafe { spawn_actor_raw(addr) }
 }
