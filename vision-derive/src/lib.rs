8a64647868b69027bbd583b99eeea3af055eb8cd
Dowland Aiello <dowlandaiello@gmail.com>
2022-10-12 16:57:43 -0700

    Change order of result derive to allow chaining proc macros.

--- vision-derive/src/lib.rs
+++ vision-derive/src/lib.rs
@@ -52,6 +52,36 @@ pub fn with_result_message(_args: TokenStream, input: TokenStream) -> TokenStrea
 		.collect();
 
 	let expanded = quote! {
+		pub fn #msg_ident(#args) {
+			use wasmer::{WasmPtr, Array, FromToNativeWasmType};
+			use vision_utils::actor::{send_message, address};
+
+			// Allocate a memory cell for the Ok or Err value
+			let init_size: u32 = 0;
+			let msg_kind = CString::new("allocate").expect("Invalid scheduler message kind encoding");
+			send_message(1,
+						 WasmPtr::from_native(msg_kind.as_ptr() as i32),
+						 WasmPtr::from_native((&init_size as *const u32) as i32));
+			let res_buf = ALLOC_RESULT.write().unwrap().take().unwrap().unwrap();
+			let ref_res_buf = WasmPtr::from_native((&res_buf as *const u32) as i32);
+
+			// Return the address of the cell to the caller
+			match #inner_ident(#arg_names) {
+				Ok(v) => {
+					let handler_name = CString::new(format!("{}_ok", #msg_name)).expect("Invalid scheduler message kind encoding");
+
+					write_t_bytes(v, res_buf);
+					send_message(from, WasmPtr::from_native(handler_name.as_ptr() as i32), ref_res_buf);
+				},
+				Err(e) => {
+					let handler_name = CString::new(format!("{}_err", #msg_name)).expect("Invalid scheduler message kind encoding");
+
+					write_t_bytes(e, res_buf);
+					send_message(from, WasmPtr::from_native(handler_name.as_ptr() as i32), ref_res_buf);
+				},
+			};
+		}
+
 		use std::{ffi::CString, sync::RwLock};
 		use serde::Serialize;
 
@@ -95,36 +125,6 @@ pub fn with_result_message(_args: TokenStream, input: TokenStream) -> TokenStrea
 							 WasmPtr::from_native(msg_kind.as_ptr() as i32),
 							 WasmPtr::from_native((&write_args as *const u8) as i32));
 			}
-		}
-
-		pub fn #msg_ident(#args) {
-			use wasmer::{WasmPtr, Array, FromToNativeWasmType};
-			use vision_utils::actor::{send_message, address};
-
-			// Allocate a memory cell for the Ok or Err value
-			let init_size: u32 = 0;
-			let msg_kind = CString::new("allocate").expect("Invalid scheduler message kind encoding");
-			send_message(1,
-						 WasmPtr::from_native(msg_kind.as_ptr() as i32),
-						 WasmPtr::from_native((&init_size as *const u32) as i32));
-			let res_buf = ALLOC_RESULT.write().unwrap().take().unwrap().unwrap();
-			let ref_res_buf = WasmPtr::from_native((&res_buf as *const u32) as i32);
-
-			// Return the address of the cell to the caller
-			match #inner_ident(#arg_names) {
-				Ok(v) => {
-					let handler_name = CString::new(format!("{}_ok", #msg_name)).expect("Invalid scheduler message kind encoding");
-
-					write_t_bytes(v, res_buf);
-					send_message(from, WasmPtr::from_native(handler_name.as_ptr() as i32), ref_res_buf);
-				},
-				Err(e) => {
-					let handler_name = CString::new(format!("{}_err", #msg_name)).expect("Invalid scheduler message kind encoding");
-
-					write_t_bytes(e, res_buf);
-					send_message(from, WasmPtr::from_native(handler_name.as_ptr() as i32), ref_res_buf);
-				},
-			};
 		}
 
 		#input
--- vision-derive/vision-derive-test/src/lib.rs
+++ vision-derive/vision-derive-test/src/lib.rs
@@ -2,6 +2,7 @@ use vision_derive::with_result_message;
 use vision_utils::types::Address;
 
 #[with_result_message]
+#[wasm_bindgen::prelude::wasm_bindgen]
 pub fn handle_mymessage(from: Address, arg: u8) -> Result<u8, String> {
 	Ok(arg)
 }
