05a89b7495a7cf1d3a6e24bf0f4c07142f77c955
Dowland Aiello <dowlandaiello@gmail.com>
2022-10-13 15:46:30 -0700

    Generate cheap in-place value type destructuring for prims.

--- vision-derive/src/lib.rs
+++ vision-derive/src/lib.rs
@@ -1,9 +1,10 @@
+use itertools::Itertools;
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
 use syn::{
-	parse, punctuated::Punctuated, token::Comma, Expr, ExprPath, FnArg, Ident, ItemFn, Pat,
-	PatIdent, Path, PathArguments, PathSegment, ReturnType, Type, GenericArgument,
+	parse, punctuated::Punctuated, token::Comma, Expr, ExprPath, FnArg, GenericArgument, Ident,
+	ItemFn, Pat, PatIdent, Path, PathArguments, PathSegment, ReturnType, Type,
 };
 
 /// Generates handle_msgname_ok and handle_msgname_err emitters for the function, providing
@@ -51,46 +52,110 @@ pub fn with_result_message(_args: TokenStream, input: TokenStream) -> TokenStrea
 		})
 		.collect();
 
-	let result_types = match input.sig.output {
+	// Use serde_json to serialize the Ok and Err values, or inline them if
+	// they are native wasm values
+	let (ok_ser, err_ser) = match input.sig.output {
 		ReturnType::Default => None,
-		ReturnType::Type(_, ty) => Some(ty),
+		ReturnType::Type(_, ref ty) => Some(ty),
 	}
-	.and_then(|ty| match *ty {
+	.and_then(|ty| match &**ty {
 		Type::Path(p) => Some(p),
 		_ => None,
 	})
-	.and_then(|p| p.path.segments.last())
-	.and_then(|s| match s.arguments {
-		PathArguments::AngleBracketed(br) => Some(br.args),
-		_ => None,
-	}).and_then(|args| args.iter().filter_map(|arg| match arg {
-		GenericArgument::Type()
-	}));
+	.and_then(|p| {
+		p.path.segments.last().and_then(|s| match &s.arguments {
+			PathArguments::AngleBracketed(br) => Some(br.args.clone()),
+			_ => None,
+		})
+	})
+	.and_then(|args| {
+		args.iter()
+			.map(
+				// If a type argument is provided to the Result returned, and it is a
+				// primitive accepted by FromToNativeWasm, generate inline serialization
+				// for it. Otherwise, use serde
+				|arg| {
+					match arg {
+						GenericArgument::Type(ty) => Some(ty),
+						_ => None,
+					}
+					.and_then(|ty| match ty {
+						Type::Path(pat) => pat.path.get_ident(),
+						_ => None,
+					})
+					.and_then(|ty_ident| match ty_ident.to_string().as_str() {
+						"i8" | "u8" | "i16" | "u16" | "i32" | "u32" | "i64" | "u64" => {
+							Some(quote! {
+								let arg = WasmPtr::from_native((&e as *const u8) as i32);
+							})
+						}
+						_ => None,
+					})
+					.unwrap_or(quote! {
+						// Allocate a memory cell for the Ok or Err value
+						let init_size: u32 = 0;
+						let msg_kind = CString::new("allocate").expect("Invalid scheduler message kind encoding");
+						send_message(1,
+									 WasmPtr::from_native(msg_kind.as_ptr() as i32),
+									 WasmPtr::from_native((&init_size as *const u32) as i32));
+						let res_buf = ALLOC_RESULT.write().unwrap().take().unwrap().unwrap();
 
-	// Use serde_json to serialize the Ok and Err values, or inline them if
-	// they are native wasm values
-	let serializer = quote! {};
+						use serde_json::to_vec;
+						use vision_utils::actor::{send_message, address};
+						use wasmer::{WasmPtr, FromToNativeWasmType};
+
+						let v_bytes = to_vec(&e).unwrap();
+
+						let msg_kind = CString::new("grow").expect("Invalid scheduler message kind encoding");
+						let msg_len = v_bytes.len();
+
+						send_message(res_buf,
+									 WasmPtr::from_native(msg_kind.as_ptr() as i32),
+									 WasmPtr::from_native((&msg_len as *const usize) as i32));
+
+						let msg_kind = CString::new("write").expect("Invalid scheduler message kind encoding");
+
+						for (i, b) in v_bytes.into_iter().enumerate() {
+							// Space for offset u32, and val u8
+							let offset: [u8; 4] = (i as u32).to_le_bytes();
+							let mut write_args: [u8; 5] = [0, 0, 0, 0, b];
+
+							for (i, b) in offset.into_iter().enumerate() {
+								write_args[i] = b;
+							}
+
+							send_message(res_buf,
+										 WasmPtr::from_native(msg_kind.as_ptr() as i32),
+										 WasmPtr::from_native((&write_args as *const u8) as i32));
+						}
+
+						let arg = WasmPtr::from_native((&res_buf as *const u32) as i32);
+					})
+				},
+			)
+			.collect_tuple()
+	})
+	.expect("Handler needs a Result<Ok, Err> return to be eligible.");
 
 	let expanded = quote! {
 		pub fn #msg_ident(#args) {
 			use wasmer::{WasmPtr, Array, FromToNativeWasmType};
 			use vision_utils::actor::{send_message, address};
 
-			// Allocate a memory cell for the Ok or Err value
-			let init_size: u32 = 0;
-			let msg_kind = CString::new("allocate").expect("Invalid scheduler message kind encoding");
-			send_message(1,
-						 WasmPtr::from_native(msg_kind.as_ptr() as i32),
-						 WasmPtr::from_native((&init_size as *const u32) as i32));
-			let res_buf = ALLOC_RESULT.write().unwrap().take().unwrap().unwrap();
 
 			// Return the address of the cell to the caller
 			match #inner_ident(#arg_names) {
-				Ok(v) => {
-					trigger_callback(v, "ok", #msg_name, res_buf);
+				Ok(e) => {
+					#ok_ser
+
+					let handler_name = CString::new(format!("{}_ok", #msg_name)).expect("Invalid scheduler message kind encoding");
+					send_message(from, WasmPtr::from_native(handler_name.as_ptr() as i32), arg);
 				},
 				Err(e) => {
-					trigger_callback(e, "err", #msg_name, res_buf);
+					#err_ser
+
+					let handler_name = CString::new(format!("{}_err", #msg_name)).expect("Invalid scheduler message kind encoding");
+					send_message(from, WasmPtr::from_native(handler_name.as_ptr() as i32), arg);
 				},
 			};
 		}
@@ -99,14 +164,6 @@ pub fn with_result_message(_args: TokenStream, input: TokenStream) -> TokenStrea
 		use serde::Serialize;
 
 		static ALLOC_RESULT: RwLock<Option<Result<Address, Address>>> = RwLock::new(None);
-
-		fn trigger_callback(v: impl Serialize, label: &str, msg_name: &str, res_buf: Address) {
-			let handler_name = CString::new(format!("{}_{}", msg_name, label)).expect("Invalid scheduler message kind encoding");
-			let ref_res_buf = WasmPtr::from_native((&res_buf as *const u32) as i32);
-
-			write_t_bytes(v, res_buf);
-			send_message(from, WasmPtr::from_native(handler_name.as_ptr() as i32), ref_res_buf);
-		}
 
 		#[wasm_bindgen::prelude::wasm_bindgen]
 		pub fn handle_allocate_ok(addr: Address) {
@@ -117,38 +174,6 @@ pub fn with_result_message(_args: TokenStream, input: TokenStream) -> TokenStrea
 		#[wasm_bindgen::prelude::wasm_bindgen]
 		pub fn handle_allocate_err(addr: Address) {
 			ALLOC_RESULT.write().unwrap().replace(Err(addr));
-		}
-
-		// Serialize the Ok or Err value, and write it to the cell
-		fn write_t_bytes(v: impl Serialize, buf: Address) {
-			use serde_json::to_vec;
-			use vision_utils::actor::{send_message, address};
-			use wasmer::{WasmPtr, FromToNativeWasmType};
-
-			let v_bytes = to_vec(&v).unwrap();
-
-			let msg_kind = CString::new("grow").expect("Invalid scheduler message kind encoding");
-			let msg_len = v_bytes.len();
-
-			send_message(buf,
-						 WasmPtr::from_native(msg_kind.as_ptr() as i32),
-						 WasmPtr::from_native((&msg_len as *const usize) as i32));
-
-			let msg_kind = CString::new("write").expect("Invalid scheduler message kind encoding");
-
-			for (i, b) in v_bytes.into_iter().enumerate() {
-				// Space for offset u32, and val u8
-				let offset: [u8; 4] = (i as u32).to_le_bytes();
-				let mut write_args: [u8; 5] = [0, 0, 0, 0, b];
-
-				for (i, b) in offset.into_iter().enumerate() {
-					write_args[i] = b;
-				}
-
-				send_message(buf,
-							 WasmPtr::from_native(msg_kind.as_ptr() as i32),
-							 WasmPtr::from_native((&write_args as *const u8) as i32));
-			}
 		}
 
 		#input
