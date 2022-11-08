47f1676778c1b37e26a7fc6b81cba5720dc628d3
Dowland Aiello <dowlandaiello@gmail.com>
2022-11-08 11:20:28 -0800

    Fix hard-coded allocator address.

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -2,6 +2,6 @@
 pub type Address = u32;
 
 /// Addresses of important root services.
-pub const PERM_ADDR: Address = 0;
-pub const ALLOCATOR_ADDR: Address = 1;
-pub const LOGGER_ADDR: Address = 2;
+pub const PERM_ADDR: Address = 1;
+pub const ALLOCATOR_ADDR: Address = 2;
+pub const LOGGER_ADDR: Address = 3;
