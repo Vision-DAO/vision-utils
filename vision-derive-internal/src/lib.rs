e27a56f80ff346a13202cda68fc221c4f01fdae6
Dowland Aiello <dowlandaiello@gmail.com>
2022-11-12 16:28:44 -0800

    See previous.

--- vision-derive-internal/src/lib.rs
+++ vision-derive-internal/src/lib.rs
@@ -439,7 +439,7 @@ pub fn with_bindings(args: TokenStream, input: TokenStream) -> TokenStream {
 		#input
 	};
 
-	println!("{:?}", arg_names);
+	println!("{}", arg_names.len());
 	let args_ptr = arg_names[1].clone();
 
 	let msg_name_ident = Ident::new(msg_name, Span::call_site());
