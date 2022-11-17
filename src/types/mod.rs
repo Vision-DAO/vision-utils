0792788420bc98f2da4c0f50ae6ece14aa7d3534
Dowland Aiello <dowlandaiello@gmail.com>
2022-11-16 20:50:44 -0800

    Allow callback to be coerced.

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{ops::Deref, sync::Arc};
 
 /// The index of an actor in the VVM.
 pub type Address = u32;
@@ -15,5 +15,19 @@ pub struct Callback<T>(Arc<Box<dyn Fn(T) + Send + Sync>>);
 impl<T> From<&'static (dyn Fn(T) + Send + Sync)> for Callback<T> {
 	fn from(cb: &'static (dyn Fn(T) + Send + Sync)) -> Callback<T> {
 		Callback(Arc::new(Box::new(cb)))
+	}
+}
+
+impl<T> AsRef<dyn Fn(T) + Send + Sync> for Callback<T> {
+	fn as_ref(&self) -> &(dyn Fn(T) + Send + Sync + 'static) {
+		(*self.0).as_ref()
+	}
+}
+
+impl<T> Deref for Callback<T> {
+	type Target = dyn Fn(T) + Send + Sync;
+
+	fn deref(&self) -> &Self::Target {
+		self.as_ref()
 	}
 }
