ae83e6e635c11f397466be8fb5f956c10131a9b4
Dowland Aiello <dowlandaiello@gmail.com>
2022-11-20 16:55:17 -0800

    See previous.

--- vision-derive-internal/src/lib.rs
+++ vision-derive-internal/src/lib.rs
@@ -155,7 +155,8 @@ pub fn with_bindings(args: TokenStream, input: TokenStream) -> TokenStream {
 				// No work needs to be done for copy types, since they are passed in as their values
 				"Address" | "i8" | "u8" | "i16" | "u16" | "i32" | "u32" | "i64" | "u64" => {
 					der = quote! {
-						let #pat = Some(#pat as #ty);
+						let #pat = #pat as #ty;
+						let #pat = Some(#pat);
 
 						{
 							#der
