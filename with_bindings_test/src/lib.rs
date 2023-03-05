bcc539b91c1f0544a937157cc77a409b5de9e453
Dowland Aiello <dowlandaiello@gmail.com>
2023-03-04 17:05:24 -0800

    Revert "Add a test project for with_bindings."
    This reverts commit 75e19c1c54a4bfbdbe9dd7e26a3d278c872ff890.

--- with_bindings_test/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use vision_derive::with_bindings;
-use vision_utils::types::{Address, Callback};
-
-#[no_mangle]
-#[with_bindings]
-pub extern "C" fn handle_test(from: Address, v: String, callback: Callback<String>) {
-	callback.call(String::from("hello there"));
-}
